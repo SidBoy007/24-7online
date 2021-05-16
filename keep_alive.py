532a94dc69f08f258f665f6a1e18cb382ea1d40c
SidBoy007 <84256617+SidBoy007@users.noreply.github.com>
2021-05-16 07:57:04 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return "Your ID is now Active 24/7! Do join our Server and Subscribe our channel if you want to Watch More Such Videos!"
+    return "Your ID is now Active 24/7! Coded By SIDBOY !!!!!!!!"
 
 def run():
     app.run(host="0.0.0.0", port=8080)
